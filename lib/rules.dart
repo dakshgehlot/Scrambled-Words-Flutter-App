cbb423717c56d7d7e9c1fd4fe10a38c2c3c44bce
TheOathkeeper <dakshgehlot30@gmail.com>
2023-01-12 20:13:24 +0530

    Updates

--- lib/rules.dart
+++ lib/rules.dart
@@ -45,7 +45,7 @@ class RulesPage extends StatelessWidget {
                           ),
                           const Text(
                             "\n1) Scrambled words will be displayed and the player has to unscramble them in the given field."
-                                "\n\n2) There is also a score counter present. For each correct answer, the score will be incremented by 10"
+                                "\n\n2) There is a score counter present. For each correct answer, the score will be incremented by 10."
                             "\n\n3) A scrambled word can be skipped by submitting an empty text field."
                                 "\n\n4) If the answer is incorrect or the word has been skipped, the score will be decremented by 5."
                                 "\n\n5) Your goal: Earn 120 score points and get the clue."
